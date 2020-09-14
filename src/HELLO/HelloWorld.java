1e74d02ec7e30e3ae56266144077d1439e748113
tayakorndev00597 <tayakorndev00597@gmail.com>
2020-09-14 15:23:54 +0700

    2nd commit

--- src/HELLO/HelloWorld.java
+++ src/HELLO/HelloWorld.java
@@ -3,5 +3,6 @@ package HELLO;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("mr.tayakorn thamlek ");
     }
 }
