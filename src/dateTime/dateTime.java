f08f7b29de618fd02925d5c27c46c9aeda3dc45a
tayakorndev00597 <tayakorndev00597@gmail.com>
2020-09-14 16:22:30 +0700

    4 commit

---
+++ src/dateTime/dateTime.java
@@ -0,0 +1,23 @@
+package dateTime;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class dateTime {
+    public static void main(String[] args) {
+        int day,month,year;
+        int second,minute,hour;
+
+        GregorianCalendar date = new GregorianCalendar();
+        day = date.get(Calendar.DAY_OF_MONTH);
+       month= date.get(Calendar.MONTH);
+       year = date.get(Calendar.YEAR);
+
+       second = date.get(Calendar.SECOND);
+       minute = date.get(Calendar.MINUTE);
+       hour = date.get(Calendar.HOUR);
+
+        System.out.println("Date is "+ day +"/"+(month+1)+"/"+year);
+        System.out.println("Time is "+ hour +":"+minute+":"+second);
+    }
+}
