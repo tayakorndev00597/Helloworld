f9715510f4067d965ad3a54165b2b31dccd22bd0
tayakorndev00597 <tayakorndev00597@gmail.com>
2020-09-14 17:11:43 +0700

    7 commit

---
+++ src/Overloading2/Overloading2.java
@@ -0,0 +1,29 @@
+package Overloading2;
+class First{
+    int x,y;
+    First(int a,int b){
+        x=a;
+        y=b;
+    }
+    void display(){
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+    }
+}
+class Second extends First {
+    int z ;
+    Second(int a,int b,int c){
+        super(a,b);
+        z = c ;
+    }
+    void display() {
+        super.display();
+        System.out.println("z: "+z);
+    }
+}
+public class Overloading2 {
+    public static void main(String[] args) {
+        Second sc =new Second(100,200,300);
+        sc.display();
+    }
+}
